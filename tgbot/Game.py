a97568fe5cecffe6f960dc329e23b214999b50e9
Miguel Barraza <miguelbarraza2015@gmail.com>
2020-04-06 15:30:04 -0300

    se extrae los textos del juego

--- tgbot/Game.py
+++ tgbot/Game.py
@@ -2,12 +2,12 @@
 # -*- encoding: utf-8 -*-
 #
 # Copyright 2020: Miguel Barraza
-
 class Game():
   def __init__(self, bot):
     self.bot = bot
     self.data = {}
     self.user = {}
+    self.history = {}
   
   def newUser(self, id):
     self.user = {}
@@ -17,6 +17,13 @@ class Game():
   def send(self, text):
     self.bot.send(self.data["id"], text)
   
+  def sendText(self, key):
+    self.send(self.history[key])
+  
   def setCommand(self, key, value, *args):
     self.user["command"][key] = (value, args)
+    self.bot.saveUser()
+  
+  def removeCommand(self, key):
+    del self.user["command"][key]
     self.bot.saveUser()
